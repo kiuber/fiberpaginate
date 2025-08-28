f6943e44ecff227f7ad201c213e3f5b562e3137d
kiuber <kiuber.zhang@gmail.com>
2025-08-28 14:54:44 +0800

    chore: make PageInfoKey public for testing

--- paginate.go
+++ paginate.go
@@ -10,7 +10,7 @@ type contextKey byte
 
 // The keys for the values in context
 const (
-	pageInfoKey contextKey = 0
+	PageInfoKey contextKey = 0
 )
 
 func New(config ...Config) fiber.Handler {
@@ -25,7 +25,7 @@ func New(config ...Config) fiber.Handler {
 
 		limit := fiber.Query[int](c, cfg.LimitKey, cfg.DefaultLimit)
 
-		c.Locals(pageInfoKey, NewPageInfo(page, limit))
+		c.Locals(PageInfoKey, NewPageInfo(page, limit))
 
 		return c.Next()
 	}
@@ -35,7 +35,7 @@ func New(config ...Config) fiber.Handler {
 // If there is a PageInfo in the context, it is returned and the boolean is true.
 // If there is no PageInfo in the context, nil is returned and the boolean is false.
 func FromContext(c fiber.Ctx) (*PageInfo, bool) {
-	if fiberpaginate, ok := c.Locals(pageInfoKey).(*PageInfo); ok {
+	if fiberpaginate, ok := c.Locals(PageInfoKey).(*PageInfo); ok {
 		return fiberpaginate, true
 	}
 	return nil, false
@@ -44,7 +44,7 @@ func FromContext(c fiber.Ctx) (*PageInfo, bool) {
 // FromContextSafe returns the PageInfo from the context.
 // If there is no PageInfo in the context, a default PageInfo is returned and the boolean is true.
 func FromContextSafe(c fiber.Ctx) (*PageInfo, bool) {
-	if c == nil || c.Locals(pageInfoKey) == nil {
+	if c == nil || c.Locals(PageInfoKey) == nil {
 		return NewPageInfo(ConfigDefault.DefaultPage, ConfigDefault.DefaultLimit), true
 	}
 	return FromContext(c)
